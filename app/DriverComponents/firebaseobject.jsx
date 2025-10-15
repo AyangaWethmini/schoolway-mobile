43c7bf5b96396c9db0ac0386c69f23ee5f069fe3
duleepa-a <duleepa24@gmail.com>
2025-10-16 00:33:22 +0530

    fix

---
+++ app/DriverComponents/firebaseobject.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4xoMbeM08mLP-qJvvFsvDchDWp0s08yw",
+  authDomain: "schoolway-4b95d.firebaseapp.com",
+  databaseURL: "https://schoolway-4b95d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "schoolway-4b95d",
+  storageBucket: "schoolway-4b95d.firebasestorage.app",
+  messagingSenderId: "682206074839",
+  appId: "1:682206074839:web:ada0e62a5bf6a22b069d92"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
